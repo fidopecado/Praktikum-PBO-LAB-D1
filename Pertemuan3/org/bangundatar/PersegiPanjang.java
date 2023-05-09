2d0b04aa2ce17762b5586f4344b8f55997f3e795
fidopecado <91780799+fidopecado@users.noreply.github.com>
2023-05-09 19:38:31 +0700

    Add files via upload

---
+++ Pertemuan3/org/bangundatar/PersegiPanjang.java
@@ -0,0 +1,22 @@
+
+//NAMA	: Firdaus Ravi Faizzani
+//NIM	: 24060121140067
+
+
+package org.bangundatar;
+import org.poligon.Poligon;
+
+public class PersegiPanjang extends Poligon{
+	private double panjang, lebar;
+	public PersegiPanjang(double panjang, double lebar, int jumlahSisi){
+		this.panjang = panjang;
+		this.lebar = lebar;
+		this.jumlahSisi = jumlahSisi;
+	}
+	public double hitungLuas(){
+		return panjang * lebar;
+	}
+	public void printInfo(){
+		System.out.println("Bangun Persegi Panjang bersisi "+this.getJumlahSisi());
+	}
+}
---
+++ Pertemuan3/org/poligon/Poligon.java
@@ -0,0 +1,11 @@
+//NAMA	: Firdaus Ravi Faizzani
+//NIM	: 24060121140067
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
