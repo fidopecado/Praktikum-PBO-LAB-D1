612caa12ed4f2811738d9064b9cf86a29cd4c408
Firdaus Ravi Faizzani <91780799+fidopecado@users.noreply.github.com>
2023-06-07 17:37:34 +0700

    Create Map.java

---
+++ Praktikum 8/Map/Map.java
@@ -0,0 +1,26 @@
+package Map;
+
+/*
+	Nama		: Firdaus Ravi Faizzani
+	NIM			: 24060121140035
+	Tanggal		: 31 Mei 2023
+*/
+
+import java.util.*;
+
+public class MapTest {
+	public static void main(String[] args) {
+		Map<Integer,String> map = new HashMap<Integer,String>();
+		//mengassign elemen kunci dan nilai 
+		map.put(01, "Juni");
+		map.put(05, "Maret");
+		//mengambil elemen pertama
+		System.out.println(map.get(01));
+		//mengambil keseluruhan kunci sebagai objek collection set
+		Set<Integer> keys = map.keySet();
+		//iterasi untuk menampilkan keseluruhan nilai collection set berdasarkan kuncinya
+		for(Integer key: keys){
+			System.out.println(key + " : " + map.get(key));
+		}
+	}   
+}
