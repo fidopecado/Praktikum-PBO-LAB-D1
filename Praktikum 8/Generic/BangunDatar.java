35e769df78d42dcbc85fde29ba1ca57937555b0f
Firdaus Ravi Faizzani <91780799+fidopecado@users.noreply.github.com>
2023-06-07 17:36:35 +0700

    Create BangunDatar.java

---
+++ Praktikum 8/Generic/BangunDatar.java
@@ -0,0 +1,11 @@
+package Generic;
+
+/*
+	Nama		: Firdaus Ravi Faizzani
+	NIM			: 24060121140035
+	Tanggal		: 31 Mei 2023
+*/
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
