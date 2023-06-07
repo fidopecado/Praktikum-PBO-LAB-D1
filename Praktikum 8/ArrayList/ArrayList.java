d2dcac15fb16e92e43a747297d67806a54863e00
Firdaus Ravi Faizzani <91780799+fidopecado@users.noreply.github.com>
2023-06-07 17:35:47 +0700

    Create ArrayList.java

---
+++ Praktikum 8/ArrayList/ArrayList.java
@@ -0,0 +1,27 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+/*
+	Nama		: Firdaus Ravi Faizzani
+	NIM			: 24060121140035
+	Tanggal		: 31 Mei 2023
+*/
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+		//inisialisasi ArrayList yang hanya dapat berisi objek string
+        ArrayList<String> arrString = new ArrayList<String>();
+        // menambahkan elemen
+        arrString.add("Nasi");
+        arrString.add("Sate");
+        arrString.add("dan Esteh");
+        // menghapus elemen arrString
+        arrString.remove("Sate");
+		//iterasi untuk menampilkan pada keseluruhan isi ArrayList
+        for(String s : arrString){
+            System.out.print(s+" ");
+        }
+        
+    }
+}
